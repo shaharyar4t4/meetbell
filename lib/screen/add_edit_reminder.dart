b1836906ac9f5a594ce83ede8e16d01d2d63adf8
Shaharyar <shaharyar4t4@gmail.com>
2025-06-06 19:29:50 +0500

    updated

--- lib/screen/add_edit_reminder.dart
+++ lib/screen/add_edit_reminder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meetbell/database/database_helper.dart';
 
 class AddEditReminder extends StatefulWidget {
   final int? reminderId;
@@ -13,9 +14,79 @@ class _AddEditReminderState extends State<AddEditReminder> {
 
   TextEditingController _titleController = TextEditingController();
   TextEditingController _descriptionController = TextEditingController();
+  String _category = "Meeting";
+  DateTime _reminderTime = DateTime.now();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (widget.reminderId != null) {
+      fetchReminder();
+    }
+  }
+
+  Future<void> fetchReminder() async {
+    try {
+      final data = await DbHepler.getRemindersById(widget.reminderId!);
+      if (data != null) {
+        _titleController.text = data['title'];
+        _descriptionController.text = data['description'];
+        _category = data['category'];
+        _reminderTime = DateTime.parse(data['reminderTime']);
+      }
+    } catch (e) {}
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.teal),
+        backgroundColor: Colors.white,
+        title: Text(
+          widget.reminderId == null ? "Add Reminder" : "Edit Reminder",
+          style: TextStyle(color: Colors.teal),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          key: _fromKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInputCard({
+    required String label,
+    required IconData icon,
+    required Widget child,
+  }) {
+    return Card(
+      elevation: 6,
+      color: Colors.teal.shade50,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(icon, color: Colors.teal),
+                SizedBox(width: 10),
+                Text(label, style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
   }
 }
